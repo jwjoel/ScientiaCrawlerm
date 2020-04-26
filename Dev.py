32f93eb28096ef3eebc94c7edcaa21cd11d730cf
Jovel <jw@Jovel.net>
2020-04-26 23:27:03 +0800

    TimeScope Backend

---
+++ Dev.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request
+import requests
+import sys
+import io
+from lxml import etree
+import urllib.parse
+import pandas as pd
+import json
+import pymysql
+from tableRequest.getTable import getTable
+from basicAccount.bindUni import bind
+from basicAccount.wxLogin import wxLogin
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'TimeScope | Ver 3.0'
+
+app.register_blueprint(getTable)
+app.register_blueprint(bind)
+app.register_blueprint(wxLogin)
+
+if __name__ == '__main__':
+
+    app.run('0.0.0.0', port=80)
